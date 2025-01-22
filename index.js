499e48c9bb1f070589e5707740e8b6280229d016
rishitrain <rishitraining245@gmail.com>
2025-01-22 17:44:34 +0530

    changes

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const mysql = require('mysql');
  const app = express();
 
  const db = mysql.createConnection({
-  host: 'localhost',         // Your MySQL server host
-  user: 'root',              // MySQL username
-  password: 'your_new_password', // MySQL password
-  database: 'db1'            // Database name
+  host: 'localhost',          
+  user: 'root',               
+  password: 'your_new_password',  
+  database: 'db1'            
 });
 
  db.connect((err) => {
@@ -15,24 +15,5 @@ const mysql = require('mysql');
     console.error('Error connecting to MySQL: ', err.stack);
     return;
   }
-  console.log('Connected to MySQL as id ' + db.threadId);
-});
-
- app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
- app.get('/users', (req, res) => {
-   db.query('SELECT * FROM users', (err, results) => {
-    if (err) {
-      console.error('Error fetching data from database:', err);
-      res.status(500).send('Database query error');
-      return;
-    }
-    res.json(results);  
-  });
-});
-
- app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Connected to MySQL as id ');
 });
