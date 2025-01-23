d033b0ba7fd11d6e0e3fbc5d61bfd58aee186d6c
rishitrain <rishitraining245@gmail.com>
2025-01-23 17:43:20 +0530

    promises and async

--- async-programing.mjs
+++ async-programing.mjs
@@ -1,15 +1,38 @@
-//async-await and fetch
-import fetch from 'node-fetch';  
-async function getDataFromAPI() {
-  try {
-    const response = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json');
-    const data = await response.json();
-    console.log(data);
-  } catch (err) {
-    console.error('Error fetching data:', err);
-  }
+// //async-await and fetch
+// import fetch from 'node-fetch';  
+// import { resolve } from 'path';
+// async function getDataFromAPI() {
+//   try {
+//     const response = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json');
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (err) {
+//     console.error('Error fetching data:', err);
+//   }
+// }
+
+// getDataFromAPI();
+
+
+
+//how the async use the promises . 
+//async solves the chaining we do in promises for resolving .
+
+//basically async function return a promise 
+
+function fetchData(){
+  return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+      resolve('Data fetched');
+    }, 2000);
+  })
 }
 
-getDataFromAPI();
+async function main() {
+  console.log("fetching data");
 
+  const res = await fetchData();
+    console.log(res);
+}
 
+main();
