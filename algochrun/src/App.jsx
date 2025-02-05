224fb527b69509dae752f7fd1e4b87d42f2ded7c
rishitrain <rishitraining245@gmail.com>
2025-02-05 16:32:54 +0530

    frontend questions

---
+++ algochrun/src/App.jsx
@@ -0,0 +1,14 @@
+ import React from 'react'
+ import Document from '../src/questions/Document'
+ import Checkbox from './questions/Checkbox'
+ 
+ function App() {
+   return (
+    <>
+    {/* <Document/> */}
+    <Checkbox/>
+    </>
+   )
+ }
+ 
+ export default App
