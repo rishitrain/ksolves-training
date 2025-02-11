febae9914b010c70ea7de6ed2497f446e5d345aa
rishitrain <rishitraining245@gmail.com>
2025-02-11 19:02:45 +0530

    added editor in the dashboard

--- graph/frontend/src/components/Editor.jsx
+++ graph/frontend/src/components/Editor.jsx
@@ -1,6 +1,9 @@
  import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import AppBar from '@mui/material/AppBar';
+import { Typography } from '@mui/material';
+
 
 const QuillEditor = () => {
   const [content, setContent] = useState('');
@@ -14,7 +17,7 @@ const QuillEditor = () => {
       [{ 'font': [] }],
       [{ 'align': [] }],
       ['link', 'image', 'video'],
-      ['clean']
+      
     ]
   };
 
@@ -29,22 +32,31 @@ const QuillEditor = () => {
   ];
 
   return (
-    <div className="h-screen p-4">
+    <>
+
+         <AppBar>
+          <Typography variant="h5" color="inherit" component="div">
+            Rich Text Editor
+          </Typography>
+      </AppBar>
+    <div className="h-screen p-4 mt-6">
       <ReactQuill
         theme="snow"
         value={content}
         onChange={setContent}
         modules={modules}
         formats={formats}
-        className="h-[400px] mb-12"
-      />
-{/* 
-      {
-        content
-      } */}
-     <div dangerouslySetInnerHTML={{__html:content}}></div>
- 
+        className="h-[400px] w-[500px] mb-12"
+      /> 
+      
+      <div className='mt-2 p-5 ' dangerouslySetInnerHTML={{__html:content}}>
+
     </div>
+
+ 
+</div>
+</>
+
   );
 };
 
