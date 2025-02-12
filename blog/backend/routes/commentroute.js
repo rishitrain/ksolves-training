e3002f43135b59ee20e42cddd96605a67dff0799
rishitrain <rishitraining245@gmail.com>
2025-02-12 12:13:43 +0530

    changes

--- blog/backend/routes/commentroute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const CommentController = require("../controllers/CommentController");
-
-router.post("/", CommentController.addComment);
-router.get("/:blog_id", CommentController.getCommentsByBlog);
-
-module.exports = router;
--- graph/backend/routes/stockroute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const stockController = require("../controller/stockdata.controller");
-
-router.get("/stocks", stockController.getStockData);
-
-module.exports = router;
