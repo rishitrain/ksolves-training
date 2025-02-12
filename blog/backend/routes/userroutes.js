9fbe6a97d59647315cc68d3a271fdbde45df6235
rishitrain <rishitraining245@gmail.com>
2025-02-12 10:37:35 +0530

    changes

--- blog/backend/routes/userroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const UserController = require("../controllers/UserController");
-
-router.get("/", UserController.getUsers);
-router.post("/login", UserController.userLogin);
-router.post("/signup", UserController.userSignup);
-
-module.exports = router;
